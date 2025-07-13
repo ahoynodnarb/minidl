Pure dense seems to work on MNIST but not convolutions at all. something is probably wrong in the backward pass but gradient verification works still so idk

minidl/functions.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+
 import numpy as np
 
 import collections.abc as abc
@@ -9,6 +10,8 @@ from typing import Callable, Optional, Tuple, Union
 import minidiff as md
 import minidiff.ops as ops
 import minidiff.typing as mdt
+
+DEBUG = False
 
 
 def log_sum_exp(x: md.Tensor) -> md.Tensor:
@@ -351,8 +354,9 @@ class Convolve2D(ops.BinaryOpClass):
                 out_dims=self.in_dims,
                 im2col_indices=self.backward_input_indices,
             )
-            # print("conv2d grad_wrt_x")
-            # print(np.linalg.norm(grad_wrt_x))
+            if DEBUG:
+                print("conv2d grad_wrt_x")
+                print(np.linalg.norm(grad_wrt_x))
 
             return grad_wrt_x
 
@@ -388,9 +392,9 @@ class Convolve2D(ops.BinaryOpClass):
                 im2col_indices=self.backward_kern_indices,
             )
             grad_wrt_w = md.swapaxes(convolved, 0, -1)
-
-            # print("conv2d grad_wrt_w")
-            # print(np.linalg.norm(grad_wrt_w))
+            if DEBUG:
+                print("conv2d grad_wrt_w")
+                print(np.linalg.norm(grad_wrt_w))
 
             return grad_wrt_w
 
@@ -432,8 +436,9 @@ class Dropout(ops.BinaryOpClass):
             a = grad_wrt_x(
                 inputs, prob, grad, auto_scale=auto_scale, trainable=trainable
             )
-            # print("dropout grad_wrt_x")
-            # print(np.linalg.norm(a))
+            if DEBUG:
+                print("dropout grad_wrt_x")
+                print(np.linalg.norm(a))
             return a
 
         return (wrapper, None)
@@ -562,8 +567,9 @@ class BatchNormalization(ops.TernaryOpClass):
 
             grad_input = component1 + component2 + component3
 
-            # print("batchnorm grad_wrt_x")
-            # print(np.linalg.norm(grad_input))
+            if DEBUG:
+                print("batchnorm grad_wrt_x")
+                print(np.linalg.norm(grad_input))
 
             return grad_input
 
@@ -598,8 +604,9 @@ class BatchNormalization(ops.TernaryOpClass):
 
             normalized_dimensions = tuple(range(grad.ndim - 1))
             out = md.sum(grad * self.x_hat, axis=normalized_dimensions)
-            # print("batchnorm grad_wrt_gamma")
-            # print(np.linalg.norm(out))
+            if DEBUG:
+                print("batchnorm grad_wrt_gamma")
+                print(np.linalg.norm(out))
             return out
 
         def compute_grad_wrt_beta(
@@ -616,8 +623,9 @@ class BatchNormalization(ops.TernaryOpClass):
 
             normalized_dimensions = tuple(range(grad.ndim - 1))
             out = md.sum(grad, axis=normalized_dimensions)
-            # print("batchnorm grad_wrt_beta")
-            # print(np.linalg.norm(out))
+            if DEBUG:
+                print("batchnorm grad_wrt_beta")
+                print(np.linalg.norm(out))
             return out
 
         return (compute_grad_wrt_x, compute_grad_wrt_gamma, compute_grad_wrt_beta)
@@ -749,8 +757,9 @@ class MaxPooling2D(ops.BinaryOpClass):
                 flattened_grad
             )
 
-            # # print("maxpooling2d grad_wrt_x")
-            # # print(np.linalg.norm(zeros))
+            if DEBUG:
+                print("maxpooling2d grad_wrt_x")
+                print(np.linalg.norm(zeros))
 
             return zeros
 
@@ -820,8 +829,9 @@ class MeanPooling2D(ops.BinaryOpClass):
             grad_wrt_x[:, row_indices, col_indices, :] = flattened_grad / (
                 pool_size * pool_size
             )
-            # # print("meanpooling2d grad_wrt_x")
-            # # print(np.linalg.norm(grad_wrt_x))
+            if DEBUG:
+                print("meanpooling2d grad_wrt_x")
+                print(np.linalg.norm(grad_wrt_x))
             return grad_wrt_x
 
         return (compute_grad_wrt_x, None)
@@ -835,6 +845,7 @@ class CrossEntropy(ops.BinaryOpClass):
         y_pred: md.Tensor,
         from_logits: bool = False,
         smoothing: Union[int, float] = 0,
+        flag: bool = False,
     ):
         if y_true is None:
             raise ValueError("Empty ground truth array")
@@ -899,7 +910,7 @@ class CrossEntropy(ops.BinaryOpClass):
             # print(probs)
             # print(self.y_true - softmax(self.y_pred))
             # print(self.y_pred)
-            if self.flag:
+            if DEBUG:
                 print("crossentropy grad_wrt_x")
                 print(np.linalg.norm(loss_grad))
             # exit(0)

minidl/neural_networks.py
@@ -10,7 +10,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import List
 
-from minidl.layers import OptimizableLayer, Layer
+from minidl.layers import OptimizableLayer, Layer, Conv2D
 from minidl.loss_functions import LossFunction
 from minidl.optimizers import LRScheduler, Optimizer
 from minidl.utils.data import shuffle_dataset, split_batches
@@ -115,7 +115,11 @@ class NeuralNetwork:
             if not layer.trainable:
                 continue
             for layer_optimizer, param in zip(layer_optimizers, layer.params):
+                if isinstance(layer, Conv2D):
+                    print("Conv grad std: " + str(np.std(param.grad)))
+                # print(np.linalg.norm(param.grad))
                 layer_optimizer.update(param)
+                param.grad = None
 
             # layer_optimizers = self.layer_optimizers
 
@@ -174,14 +178,10 @@ class NeuralNetwork:
             )
             progress.set_description(f"Epoch #{epoch + 1}")
             self.check = False
-            for i, (x, y_true) in enumerate(progress):
+            for x, y_true in progress:
                 y_pred = self(x)
-                if i % 30 == 0:
-                    self.loss_function.flag = True
-                else:
-                    self.loss_function.flag = False
                 loss = self.loss_function(y_true, y_pred)
-                loss.backward()
+                loss.backward(cleanup_mode="destroy")
 
                 with md.no_grad():
                     self.update_layer_weights()

minidl/optimizers.py
@@ -51,7 +51,7 @@ class Adam(Optimizer):
 
         self.momentum = None
         self.velocity = None
-        self.t = 0
+        self.t = 1
 
     def update(self, param: md.Tensor, l2_lambda: float = 0):
         self.t += 1
@@ -60,7 +60,7 @@ class Adam(Optimizer):
             self.momentum = md.zeros_like(grad)
             self.velocity = md.zeros_like(grad)
 
-        regularized_grad = grad + l2_lambda * param
+        regularized_grad = grad - l2_lambda * param
 
         self.momentum = self.beta1 * self.momentum + (1 - self.beta1) * regularized_grad
         self.velocity = self.beta2 * self.velocity + (1 - self.beta2) * (
@@ -70,7 +70,11 @@ class Adam(Optimizer):
         m_hat = self.momentum / (1 - self.beta1**self.t)
         v_hat = self.velocity / (1 - self.beta2**self.t)
 
+        print("Adam m_hat:", np.mean(m_hat))  # Should be ~gradient scale
+        print("Adam v_hat:", np.mean(v_hat))  # Should be ~gradient squared
+
         w_updt = self.learning_rate * m_hat / (md.sqrt(v_hat) + self.epsilon)
+        print(np.mean(w_updt))
         param -= w_updt
 
 